2edf01bb3678a192b0d0d98dc81c27be7ee218f9
damesaa201710054010 <damesaa@eafit.edu.co>
2019-11-13 00:07:41 -0500

    hi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 require('./database');
-const ngrok = require('ngrok');
+//const ngrok = require('ngrok');
 
 async function main() {
     /*app.listen(app.get('port'), app-get('ip'));
