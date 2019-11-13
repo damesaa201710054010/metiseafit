ada1188d81f26a3c26f928da52e3a0948a3ce967
Youngermaster <jmyoungh@eafit.edu.co>
2019-11-12 19:37:27 -0500

    I fixed the `views.routes.js` absolute paths

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 const router = Router();
+const ROOT_DIR  = require('../rootDir.js');
 var path = require('path');
 
 router.route('/test')
     .get((req, res) => {
-        res.render(path.join('/home/youngermaster/AzureDevOps/METIS-AZURE/src/views/test.pug'));
+        res.render(path.join(ROOT_DIR + '/views/test.pug'));
     });
 
 router.route('/')
     .get((req, res) => {
-        res.render(path.join('/home/youngermaster/AzureDevOps/METIS-AZURE/src/views/index.pug'));
+        res.render(path.join(ROOT_DIR + '/views/index.pug'));
     });
 
 
