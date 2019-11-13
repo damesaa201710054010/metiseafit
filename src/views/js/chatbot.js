76b515a6a3fffd491b7068c1774d47d306a24459
Youngermaster <jmyoungh@eafit.edu.co>
2019-11-12 19:45:40 -0500

    Vamo' a ver

--- src/views/js/chatbot.js
+++ src/views/js/chatbot.js
@@ -80,7 +80,7 @@ function serverMessage(response2) {
 
 function fetchmsg(){
 
-  var url = 'http://localhost:5000/send-msg';
+  var url = 'https://metiseafit.azurewebsites.net/send-msg';
   
   const data = new URLSearchParams();
   for (const pair of new FormData(document.getElementById("mymsg"))) {
